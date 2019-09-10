a485a626cc6108c04a81d95c214873d5a8ef5831
Lokesh Soni <ynkshatriya@gmail.com>
2019-09-10 14:53:05 +0530

    Tue Sep 10 14:23:16 IST 2019

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,12 +5,17 @@ const kActiveCardColour = Color(0xFF1D1E33);
 const kInactiveCardColour = Color(0xFF111328);
 const kBottomContainerColour = Color(0xFFEB1555);
 
-const kLabelTextStyle = TextStyle(
-  fontSize: 18,
-  color: Color(0xFF8D8E98),
-);
+const kLabelTextStyle = TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
+const kNumberTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
 
-const kNumberTextStyle = TextStyle(
-  fontSize: 50,
-  fontWeight: FontWeight.w900,
-);
+const kLargeButtonTextStyle =
+    TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
+
+const kResultTitleTextStyle =
+    TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D976), fontSize: 22, fontWeight: FontWeight.bold);
+
+const kBMITextStyle = TextStyle(fontSize: 100, fontWeight: FontWeight.bold);
+const kBodyTextStyle = TextStyle(fontSize: 22);
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,11 +1,50 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+
+import 'bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("BMI CALCULATOR")),
-      body: Text("Hello"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text('YOUR RESULT', style: kResultTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Normal', style: kResultTextStyle),
+                  Text('18.3', style: kBMITextStyle),
+                  Text(
+                    'Your BMI result is quite low, you should eat more!.',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: "RECALCULATE",
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
     );
   }
 }
